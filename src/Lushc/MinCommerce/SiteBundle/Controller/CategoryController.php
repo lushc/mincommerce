3dcb8014691b07d51334454560cff544030d4737
Chris Lush <chris.lush@mobilefun.co.uk>
2014-05-12 12:42:05 +0100

    Add pagination to products display in category pages

--- src/Lushc/MinCommerce/SiteBundle/Controller/CategoryController.php
+++ src/Lushc/MinCommerce/SiteBundle/Controller/CategoryController.php
@@ -5,6 +5,10 @@ namespace Lushc\MinCommerce\SiteBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Pagerfanta\Pagerfanta;
+use Pagerfanta\Adapter\DoctrineCollectionAdapter;
+use Pagerfanta\Exception\NotValidCurrentPageException;
 
 class CategoryController extends Controller
 {
@@ -14,11 +18,28 @@ class CategoryController extends Controller
     */
     public function showAction($slug)
     {
+        // get the requested category and its products
         $repository = $this->getDoctrine()->getRepository('MinCommerceSiteBundle:Category');
         $category = $repository->findOneBySlug($slug);
+        $products = $category->getProducts();
+
+        // set up pagination
+        $pagerfanta = new Pagerfanta(new DoctrineCollectionAdapter($products));
+        $pagerfanta->setMaxPerPage(5);
+
+        $request = $this->getRequest();
+        $page = (is_numeric($request->query->get('page')) ? (int) $request->query->get('page') : 1);
+
+        try {
+            $pagerfanta->setCurrentPage($page);
+        }
+        catch (NotValidCurrentPageException $e) {
+            throw new NotFoundHttpException();
+        }
 
         return array(
             'category' => $category,
+            'products' => $pagerfanta,
         );
     }
 }
